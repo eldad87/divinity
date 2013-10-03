1db0d4482d7cb5b21eed1689e926bdbfb4c9b912
Eldad Yamin <eldad87@gmail.com>
2013-10-03 22:01:08 +0200

    Fix: position of entities always in 0,0

--- gameClasses/CharacterContainer.js
+++ gameClasses/CharacterContainer.js
@@ -6,24 +6,28 @@ var CharacterContainer = IgeEntityBox2d.extend({
         var self = this;
         IgeEntityBox2d.prototype.init.call(this);
 
-        // Set bounding box
-        self.box2dBody({
-            type: 'dynamic',
-            linearDamping: 0.0,
-            angularDamping: 0.1,
-            allowSleep: true,
-            bullet: false,
-            gravitic: true,
-            fixedRotation: false,
-            fixtures: [{
-                density: 1.0,
-                friction: 0.5,
-                restitution: 0.2,
-                shape: {
-                    type: 'circle'
-                }
-            }]
-        });
+        /* CEXCLUDE */
+        if (ige.isServer) {
+            // Set bounding box
+            self.box2dBody({
+                type: 'dynamic',
+                linearDamping: 0.0,
+                angularDamping: 0.1,
+                allowSleep: true,
+                bullet: false,
+                gravitic: true,
+                fixedRotation: false,
+                fixtures: [{
+                    density: 1.0,
+                    friction: 0.5,
+                    restitution: 0.2,
+                    shape: {
+                        type: 'circle'
+                    }
+                }]
+            });
+        }
+        /* CEXCLUDE */
 
         if (!ige.isServer) {
             // Setup the entity 3d bounds
